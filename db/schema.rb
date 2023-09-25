a96483611103f2fc4281f1c4c33bb11500fdfc7c
RileyManda <rileymanda69@gmail.com>
2023-09-25 13:57:59 +0200

    Updated create user migration to include posts_counter column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_101037) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_115628) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_25_101037) do
     t.text "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_counter", default: 0
   end
 
   add_foreign_key "comments", "posts"
