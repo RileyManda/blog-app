58fc408d929dc171d820e19f74530913c2603431
RileyManda <rileymanda69@gmail.com>
2023-09-25 22:25:04 +0200

    Implemented after_save and set methods to private:like|comment|post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+  after_save :update_comment_counter
+
+  private
 
   def update_comment_counter
     post.update(comments_counter: post.comments.count)
