0c97a3241b2184a60e17abc43e37cf758ab88383
RileyManda <rileymanda69@gmail.com>
2023-09-26 15:46:15 +0200

    Test:Post-tittle must not be blank

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 # test: Name must not be blank
 RSpec.describe Post, type: :model do
-  # PostsCounter must be an integer greater than or equal to zero
+  # Tittle must not be blank.
+  it 'tittle must not be blank.' do
+    post = Post.new(title: nil)
+    expect(post).not_to be_valid
+  end
 end
