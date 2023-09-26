b9bc525ea7411f8ee009bb817fbdb80f215d4eb5
RileyManda <rileymanda69@gmail.com>
2023-09-26 17:32:33 +0200

    Added licence to project

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 # test: Name must not be blank
 RSpec.describe Post, type: :model do
   # Tittle must not be blank.
-  it 'tittle must not be blank.' do
+  it 'title must not be blank.' do
     post = Post.new(title: nil)
     expect(post).not_to be_valid
   end
