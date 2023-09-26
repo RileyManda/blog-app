bd7cba0c2f0d902876e226fe83756e3dafeac182
RileyManda <rileymanda69@gmail.com>
2023-09-26 12:10:12 +0200

    Validation:PostsCounter must be an integer greater than or equal to zero

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'user_id'
   # validations: name must not be blank
   validates :name, presence: true
+  # PostsCounter must be an integer greater than or equal to zero
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
   end
