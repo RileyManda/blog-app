fe3072ce722bf7c8a7bff92b099033776cdd54d0
RileyManda <rileymanda69@gmail.com>
2023-09-26 15:29:02 +0200

    Test:postsCounter must be an integer greater than or equal to zero

---
+++ spec/post_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+# test: Name must not be blank
+RSpec.describe Post, type: :model do
+  # PostsCounter must be an integer greater than or equal to zero
+end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 # test: Name must not be blank
 RSpec.describe User, type: :model do
-  # name should not be empty
-  it 'name should bnot be empty' do
+  # Name must not be blank.
+  it 'name must not be blank.' do
     user = User.new(name: nil)
     expect(user).not_to be_valid
   end
-
+  # PostsCounter must be an integer greater than or equal to zero
+  it 'postsCounter must be an integer greater than or equal to zero' do
+    user = User.new(name: 'test', posts_counter: -1)
+    expect(user).not_to be_valid
+  end
 end
