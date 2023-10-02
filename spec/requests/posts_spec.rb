6fd01e62b58ef93b155a63f1d4edf43c252a6edb
RileyManda <rileymanda69@gmail.com>
2023-10-02 17:11:59 +0200

    Post requests specs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  let(:first_user) { User.create(name: 'John Doe', bio: 'Sample bio') }
+
   describe 'GET /posts' do
-    it 'works! (now write some real specs)' do
+    it 'returns a 200 OK response' do
       get posts_path
       expect(response).to have_http_status(200)
+    end
+
+    it "contains 'Posts List' in the response body" do
+      get posts_path
+      expect(response.body).to include('Posts List')
+    end
+    it "renders the 'posts/index' template" do
+      get posts_path
+      expect(response).to render_template('posts/index')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,4 +7,29 @@ RSpec.describe 'Users', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+  it "renders the 'users/index' template" do
+    get users_path
+    expect(response).to render_template('users/index')
+  end
+  it "contains 'Users List' in the response body" do
+    get users_path
+    expect(response.body).to include('User List')
+  end
+end
+describe 'GET #show' do
+  it 'returns a 200 OK response' do
+    user = User.create(name: 'Sample User', bio: 'Sample bio')
+    get user_path(user)
+    expect(response).to have_http_status(200)
+  end
+  it "renders the 'users/show' template" do
+    user = User.create(name: 'Sample User', bio: 'Sample bio')
+    get user_path(user)
+    expect(response).to render_template('users/show')
+  end
+  it "contains 'User Details' in the response body" do
+    user = User.create(name: 'Sample User', bio: 'Sample bio')
+    get user_path(user)
+    expect(response.body).to include('User details')
+  end
 end
