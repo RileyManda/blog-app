8627e4da9040d62a3edd26f296d9eb3465c2e61a
RileyManda <rileymanda69@gmail.com>
2023-10-02 17:30:53 +0200

    Posts request specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  let(:first_user) { User.create(name: 'John Doe', bio: 'Sample bio') }
   describe 'GET /users' do
     it 'works! (now write some real specs)' do
       get users_path
