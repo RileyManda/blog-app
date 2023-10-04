15640686202e6a82ddb74c3c9df415f1b5982345
RileyManda <rileymanda69@gmail.com>
2023-10-04 22:07:28 +0200

    Added like btn

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments
+      resources :likes, only: [:create]
     end
     get 'posts/new', to: 'posts#new', as: 'new_post'
   end
