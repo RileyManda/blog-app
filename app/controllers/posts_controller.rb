d341c296cfa4e4558aa7290b13e9ab1bdde35941
RileyManda <rileymanda69@gmail.com>
2023-10-05 14:01:00 +0200

    Updated post controller:create method of post current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @user = User.find(params[:user_id])
+    @user = current_user
     @post = Post.new(
       author: @user,
       title: params[:post][:title],
