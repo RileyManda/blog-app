2585292d470a35724e2244ca8da3e452d3e37f59
RileyManda <rileymanda69@gmail.com>
2023-10-09 19:21:30 +0200

    Generated users_show_spec

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Blogapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.assets.enabled = true
+
 
     # Configuration for the application, engines, and railties goes here.
     #
