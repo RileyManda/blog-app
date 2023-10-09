fc1d8ae4aba7a56e0d14a8242534f47a605bb8f5
RileyManda <rileymanda69@gmail.com>
2023-10-09 17:54:52 +0200

    Reduced line size

--- spec/features/users_index_spec.rb
+++ spec/features/users_index_spec.rb
@@ -3,32 +3,25 @@ require 'rails_helper'
 RSpec.feature 'Users Index Page' do
   let(:users_data) do
     [
-      {
-        name: 'Minion',
+      { name: 'Minion',
         photo: 'profile-image.png',
         bio: 'The first user in the BlogApp application.',
-        posts_counter: 2
-      },
-      {
-        name: 'Riley',
-        photo: 'profile-image.png',
-        bio: 'A second user in the BlogApp application.',
-        posts_counter: 2
-      }
+        posts_counter: 2 },
+      { name: 'Riley', photo: 'profile-image.png', bio: 'A second user in the BlogApp application.', posts_counter: 2 }
     ]
   end
 
   before do
-    users_data.each do |user_data|
-      User.create(user_data)
-    end
-
+    users_data.each { |user_data| User.create(user_data) }
     visit users_path
+  end
+
+  scenario 'expect #index to display view title' do
+    expect(page).to have_content('Users')
   end
 
   scenario 'displays user information' do
     users_data.each do |user_data|
-      # expect(page).to have_css('user-image')
       expect(page).to have_content(user_data[:name])
       expect(page).to have_content(user_data[:posts_counter])
     end
