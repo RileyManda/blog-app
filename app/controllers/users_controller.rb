f45cbbd6bc4a1fded0feb0ed6a9b140f0a5ac8da
RileyManda <rileymanda69@gmail.com>
2023-10-12 00:46:42 +0200

    Can can config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  # load_and_authorize_resource
   def index
     @users = User.all
+  end
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
   end
 
   def show
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Ability
   include CanCan::Ability
 
@@ -7,8 +9,10 @@ class Ability
 
     an(:read, Post, user:)
     return unless user.admin?
-
-    can :read, Post
+    can :create, Post
+    can :remove, Post
+    # Define abilities for the user here. For example:
+    #
     #   return unless user.present?
     #   can :read, :all
     #   return unless user.admin?
