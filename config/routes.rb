8bc8b0975cbccf8d1d8cd4dbd0f705d218322979
RileyManda <rileymanda69@gmail.com>
2023-10-12 18:10:53 +0200

    Bug fix: Comments and Posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   resources :posts
   resources :users do
     resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :index, :create]
       resources :comments, only: [:create, :destroy]
       resources :likes, only: [:create]
       post 'like', to: 'likes#create', on: :member
     end
     get 'posts/new', to: 'posts#new', as: 'new_post'
     get 'comment', to: 'posts#comment_form', on: :member
+    # get '/users/:id/posts/:id/comments', to: 'comments#index', defaults: { format: 'json' }
+
   end
    delete 'users/:user_id/posts/:id', to: 'posts#destroy', as: 'member'
 end
