2d1358e6e1dac38f3e5412c9321af4e324fa0588
RileyManda <rileymanda69@gmail.com>
2023-10-12 10:10:38 +0200

    Ability:Users can add comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,8 +7,11 @@ class Ability
     else
       can :read, :all
       can :manage, Post, author_id: user.id
-      can :manage, Comment, post: { author_id: user.id }
-      can :destroy, Comment, user_id: user.id
+      can :manage, Comment, user_id: user.id
+      can :create, Comment
+      can :destroy, Comment do |comment|
+        comment.user == user || comment.post.author == user
+      end
     end
   end
 end
