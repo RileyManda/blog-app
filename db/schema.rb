4d9e192710d5a787169f1be0fe7a40c5a5e0cf60
RileyManda <rileymanda69@gmail.com>
2023-10-14 16:59:04 +0200

    Updated confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_211120) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_13_204649) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_211120) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "role"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -1,4 +1,5 @@
 require 'swagger_helper'
+
 RSpec.describe 'Comments API' do
   path '/api/v1/comments' do
     get 'Retrieves all comments' do
@@ -13,9 +14,11 @@ RSpec.describe 'Comments API' do
                  },
                  required: %w[id text]
                }
+
         run_test!
       end
     end
+
     post 'Creates a comment' do
       tags 'Comments'
       consumes 'application/json'
@@ -26,22 +29,26 @@ RSpec.describe 'Comments API' do
         },
         required: ['text']
       }
+
       response '201', 'comment created' do
         let(:comment) { { text: 'This is a comment' } }
         run_test!
       end
+
       response '422', 'invalid request' do
         let(:comment) { { text: '' } }
         run_test!
       end
     end
   end
+
   path '/api/v1/comments/{id}' do
     # Define the 'show' endpoint documentation
     get 'Retrieves a comment' do
       tags 'Comments'
       produces 'application/json'
       parameter name: :id, in: :path, type: :string
+
       response '200', 'comment found' do
         schema type: :object,
                properties: {
@@ -49,21 +56,26 @@ RSpec.describe 'Comments API' do
                  text: { type: :string }
                },
                required: %w[id text]
+
         let(:id) { Comment.create(text: 'This is a comment').id }
         run_test!
       end
+
       response '404', 'comment not found' do
         let(:id) { 'invalid' } # Example of a non-existent comment
         run_test!
       end
     end
+
     delete 'Deletes a comment' do
       tags 'Comments'
       parameter name: :id, in: :path, type: :string
+
       response '204', 'comment deleted' do
         let(:id) { Comment.create(text: 'This is a comment').id }
         run_test!
       end
+
       response '404', 'comment not found' do
         let(:id) { 'invalid' }
         run_test!
